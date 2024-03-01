637143b74d6cd9fe89af28c66e2f9c73b25c56a5
devanshg03 <105848104+devanshg03@users.noreply.github.com>
2024-03-01 13:37:25 -0500

    force update

--- Week 1/week1-class.py
+++
@@ -1,9 +0,0 @@
-def area(base: float, height: float) -> float:
-    """Returns the area of a triangle using the given base and height.
-    
-    >>> area(10,5)
-    25.0
-    >>> area(2.2,2.4)
-    2.464
-    """
-    return base * height / 2
